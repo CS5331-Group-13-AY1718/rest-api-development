702b17e1b9fa28ab004b2c12b1d302e29a50981b
Joshua Che <joshua.chejy@gmail.com>
2018-02-28 20:37:19 +0800

    added ui for register/login users, view/create/del entries

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,12 @@ RUN pip install -U pip
 RUN pip install -U flask
 RUN pip install -U flask-cors
 RUN pip install -U pytz
+RUN pip install -U wtforms
+RUN pip install -U passlib
 RUN echo "ServerName localhost  " >> /etc/apache2/apache2.conf
 RUN echo "$user     hard    nproc       20" >> /etc/security/limits.conf
 ADD ./src/service /service
-ADD ./src/html /var/www/html
+ADD ./src/service/templates /var/www/html
 EXPOSE 80
 EXPOSE 8080
 CMD ["/bin/bash", "/service/start_services.sh"]
--- src/service/app.py
+++ src/service/app.py
@@ -3,20 +3,27 @@
 from flask import Flask
 from flask import g
 from flask import request
+from flask import render_template, flash, redirect, url_for, session, logging
 from flask_cors import CORS
 from datetime import datetime
+from wtforms import Form, StringField, TextAreaField, IntegerField, PasswordField, SelectField, validators
+from passlib.hash import sha256_crypt
 import json
 import os
 import sqlite3
 import uuid
 import pytz
+from functools import wraps
+from logging import FileHandler
 
 DATABASE = 'database.db'
-
 
 app = Flask(__name__)
 # Enable cross origin sharing for all endpoints
 CORS(app)
+
+file_handler = FileHandler('log.txt')
+app.logger.addHandler(file_handler)
 
 def get_db():
     """UTILITY METHOD"""
@@ -75,11 +82,27 @@ def make_diary_dict(row):
     diaryDict = {"id":row[0],"title":row[1],"author":row[2],"publish_date":row[3],"public":row[4],"text":row[5]}
     return diaryDict
 
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        try:
+            if session['logged_in']:
+                user_token = user_from_token(session['token'])
+                if session['username'] == user_token[0]:
+                    return f(*args, **kwargs)
+                else:
+                    return redirect(url_for('users_authenticate'))
+            else:
+                return redirect(url_for('users_authenticate'))
+        except KeyError:
+            return redirect(url_for('users_authenticate'))
+    return wrap
+
 """===========================================END OF UTILITY METHODS==========================================="""
 """===========================================START OF PYTHON (2.7) FLASK WEB APP==========================================="""
 
 # Remember to update this list
-ENDPOINT_LIST = ['/', '/meta/heartbeat', '/meta/members', '/users/register', '/users/authenticate', '/users/expire', '/users', '/diary', '/diary/create', '/diary/delete', '/diary/permission']
+ENDPOINT_LIST = ['/', '/meta/heartbeat', '/meta/members', '/users/register', '/users/authenticate', '/users/expire', '/user/logout', '/users', '/diary', '/public', '/diary/create', '/diary/delete', '/diary/permission']
 
 def make_json_response(data, status=True, code=200, username=None, fullname=None, age=None):
     """Utility function to create the JSON responses."""
@@ -110,7 +133,8 @@ def make_json_response(data, status=True, code=200, username=None, fullname=None
 @app.route("/")
 def index():
     """Returns a list of implemented endpoints."""
-    return make_json_response(ENDPOINT_LIST)
+    return render_template('index.html')
+    #make_json_response(ENDPOINT_LIST)
 
 
 @app.route("/meta/heartbeat")
@@ -126,47 +150,78 @@ def meta_members():
         team_members = f.read().strip().split("\n")
     return make_json_response(team_members)
 
+class RegisterForm(Form):
+    fullname = StringField('Full name', [validators.Length(min=1, max=50)])
+    age = IntegerField('Age', [validators.input_required()])
+    username = StringField('Username', [validators.Length(min=4, max=20)])
+    password = PasswordField('Password', [validators.DataRequired()])
 
-@app.route("/users/register", methods=['POST'])
+@app.route("/users/register", methods=['GET', 'POST'])
 def users_register():
-    if request.method == 'POST':
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate:
         """Registers users"""
-        paramsJSON = request.get_json()
-        username = paramsJSON['username']
-        password =  paramsJSON['password']
-        fullname = paramsJSON['fullname']
-        age = paramsJSON['age']
+        """Registers users"""
+        fullname = form.fullname.data
+        age = form.age.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        #paramsJSON = request.get_json()
+        #username = paramsJSON['username']
+        #password =  paramsJSON['password']
+        #fullname = paramsJSON['fullname']
+        #age = paramsJSON['age']
         try:
             query = "insert into users(username, password, fullname, age) values ('%s','%s','%s',%s)" % (username,password,fullname,str(age))
             result = query_db(query)
-            return make_json_response(None, code=201)
+            return render_template('login.html')
+            #make_json_response(None, code=201)
         except sqlite3.IntegrityError:
             result = "User already exists!"
             return make_json_response(result, status=False)
+    else:
+        return render_template('register.html', form=form)
 
 
-@app.route("/users/authenticate", methods=['POST'])
+@app.route("/users/authenticate", methods=['GET', 'POST'])
 def users_authenticate():
     if request.method == 'POST':
-        paramsJSON = request.get_json()
-        username = paramsJSON['username']
-        password =  paramsJSON['password']
+        username = request.form['username']
+        password_candidate = request.form['password']
+        #paramsJSON = request.get_json()
+        #username = paramsJSON['username']
+        #password =  paramsJSON['password']
 
         """Check if such a user exists first"""
-        query = "select * from users where username='%s'and password='%s'" % (username,password)
+        query = "select password from users where username='%s'" % (username)
         result = query_db(query)
 
         if result == []:
+            app.logger.info('%s failed to log in', username)
             return make_json_response(data=None, status=False)
-
         else:
             """Authenticates user"""
-            """Query to insert token to users table token column"""
-            generatedToken = uuid.uuid4()
-            query = "update users SET token='%s' where username='%s'and password='%s'" % (str(generatedToken), username,password)
-            result = query_db(query)
-            """An update query does not return a result in query_db()"""
-            return make_json_response(str(generatedToken))
+            password = result[0][0]
+            if sha256_crypt.verify(password_candidate, password):
+                """Query to insert token to users table token column"""
+                generatedToken = uuid.uuid4()
+                query = "update users SET token='%s' where username='%s'and password='%s'" % (str(generatedToken), username,password)
+                result = query_db(query)
+
+                session['logged_in'] = True
+                session['username'] = username
+                session['token'] = generatedToken
+
+                app.logger.info('%s logged in successfully. token = %s', session['username'], session['token'])
+
+                """An update query does not return a result in query_db()"""
+                return redirect(url_for('index')) 
+                #make_json_response(str(generatedToken))
+            else:
+                return render_template('login.html')
+    else:
+        return render_template('login.html')
 
 
 @app.route("/users/expire", methods=['POST'])
@@ -190,6 +245,11 @@ def users_expire():
             """An update query does not return a result in query_db()"""
             return make_json_response(data=None)
 
+@app.route('/users/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 @app.route("/users", methods=['GET', 'POST'])
 def users_get():
@@ -213,63 +273,86 @@ def users_get():
         else:
             return make_json_response(None, username=result[0], fullname=result[2], age=result[3])
 
-
+@app.route("/public")
+def public_get():
+    """Retrieve all public diary entries"""
+    """No req params needed, only response"""
+    query = "select * from diaries where public='1'"
+    result = query_db(query)
+    arrOfDiaryDicts = []
+    for row in result:
+        arrOfDiaryDicts.append(make_diary_dict(row))
+    return render_template('public.html', public=arrOfDiaryDicts)
+    #make_json_response(arrOfDiaryDicts))
 
 @app.route("/diary", methods=['GET', 'POST'])
+@is_logged_in
 def diary_get():
-    if request.method =='GET':
-        """Retrieve all public diary entries"""
-        """No req params needed, only response"""
-        query = "select * from diaries where public='1'"
+    #if request.method =='POST':
+    """Retrieve all entries belonging to an authenticated user"""
+    #paramsJSON = request.get_json()
+    #token = paramsJSON['token']
+    token = session['token']
+    result = user_from_token(token)
+    if result == []:
+        return make_json_response(data="Invalid authentication token", status=False)
+    else:
+        username = result[0]
+        query = "select * from diaries where author='%s'" % (username)
         result = query_db(query)
         arrOfDiaryDicts = []
         for row in result:
             arrOfDiaryDicts.append(make_diary_dict(row))
-        return make_json_response(arrOfDiaryDicts)
+        return render_template('diary.html', diary=arrOfDiaryDicts)
+        #make_json_response(arrOfDiaryDicts)
 
-    if request.method =='POST':
-        """Retrieve all entries belonging to an authenticated user"""
-        paramsJSON = request.get_json()
-        token = paramsJSON['token']
-        result = user_from_token(token)
-        if result == []:
-            return make_json_response(data="Invalid authentication token", status=False)
-        else:
-            username = result[0]
-            query = "select * from diaries where author='%s'" % (username)
-            result = query_db(query)
-            arrOfDiaryDicts = []
-            for row in result:
-                arrOfDiaryDicts.append(make_diary_dict(row))
-            return make_json_response(arrOfDiaryDicts)
+class DiaryForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=200)])
+    public = SelectField('Permission', choices=[('public', 'Public'), ('private', 'Private')])
+    body = TextAreaField('Body', [validators.Length(min=1)])
 
-
-@app.route("/diary/create", methods=['POST'])
+@app.route("/diary/create", methods=['GET', 'POST'])
+@is_logged_in
 def diary_create():
-    if request.method =='POST':
+    form = DiaryForm(request.form)
+    if request.method =='POST' and form.validate:
         """Create a new diary entry"""
-        paramsJSON = request.get_json()
-        token = paramsJSON['token']
-        title = paramsJSON['title']
-        public = paramsJSON['public']
-        text = paramsJSON['text']
-        result = user_from_token(token)
-        if result ==[]:
-            return make_json_response(data="Invalid authentication token", status=False)
+        title = form.title.data
+        text = form.body.data
+        public = form.public.data
+        username = session['username']
+        if public == 'public':
+            public = True
         else:
-            username = result[0]
-            generatedDT = get_current_datetime()
-            query = "insert into diaries ('title', 'author', 'publish_date', 'public', 'text') values ('%s','%s','%s','%d','%s')" % (title,username,generatedDT,int_from_boolean(public),text)
-            result = query_db(query)
-            return make_json_response(data=2, code=201)
+            public = False
 
+        #paramsJSON = request.get_json()
+        #token = paramsJSON['token']
+        #title = paramsJSON['title']
+        #public = paramsJSON['public']
+        #text = paramsJSON['text']
+        #result = user_from_token(token)
+        #if result ==[]:
+        #    return make_json_response(data="Invalid authentication token", status=False)
+        #else:
+        #    username = result[0]
+        generatedDT = get_current_datetime()
+        query = "insert into diaries ('title', 'author', 'publish_date', 'public', 'text') values ('%s','%s','%s','%d','%s')" % (title,username,generatedDT,int_from_boolean(public),text)
+        result = query_db(query)
+        return redirect(url_for('diary_get'))
+        #make_json_response(data=2, code=201)
+    else:
+        return render_template('diary_create.html', form=form)
 
-@app.route("/diary/delete", methods=['POST'])
-def diary_delete():
+@app.route("/diary/delete/<string:entryid>", methods=['POST'])
+def diary_delete(entryid):
     if request.method =='POST':
-        paramsJSON = request.get_json()
-        token = paramsJSON['token']
-        id = paramsJSON['id']
+        token = session['token']
+        username = session['username']
+        id = int(entryid)
+        #paramsJSON = request.get_json()
+        #token = paramsJSON['token']
+        #id = paramsJSON['id']
         """Get user from token first"""
         user = user_from_token(token)
         """If no such user with this token exists, else"""
@@ -277,6 +360,7 @@ def diary_delete():
             return make_json_response(data="Invalid authentication token", status=False)
         else:
             username = user[0]
+            app.logger.info('Deleting diary id = %d from author = %s', id, username)
             query = "delete from diaries where author='%s' and id='%d'" % (username, id)
             result = query_db(query)
             return make_json_response(None)
@@ -309,4 +393,5 @@ if __name__ == '__main__':
     os.chdir(dname)
 
     # Run the application
-    app.run(debug=False, port=8080, host="0.0.0.0")
+    app.secret_key='secret12345'
+    app.run(debug=True, port=8080, host="0.0.0.0")
