737d7b94717b09a4f12d7c6b72356674a67d8d60
Joshua Che <joshua.chejy@gmail.com>
2018-03-05 21:56:05 +0800

    user session authenticated by token, can retrieve own diaries

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 TEAMID=`md5sum README.md | cut -d' ' -f 1`
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
--- src/html/demo.js
+++ src/html/demo.js
@@ -68,7 +68,7 @@ ajax_get(API_ENDPOINT + '/meta/members', function(data) {
 ajax_get(API_ENDPOINT + '/diary', function(data) {
     if (data.status) {
         var diaries = data.result;
-        var output = "<p><h3>Public Diary Entries:</h3></p><ul>";
+        var output = "<ul>";
         for (var i = 0; i < diaries.length; i++) {
             entry = diaries[i];
             temp_date = entry.publish_date;
@@ -77,10 +77,10 @@ ajax_get(API_ENDPOINT + '/diary', function(data) {
             output += "<li>";
             output += "<strong>" + entry.title + "</strong>";
             output += "<br>";
-            output += "<small>Written on " + publish_date + " at " + publish_time + "</small>";
+            output += "<small>Written by " + entry.author + " on " + publish_date + " at " + publish_time + "</small>";
             output += "<hr>" + entry.text + "</li>";
         }
-        output += "</ul>";
+        output += "</ul><br>";
         document.getElementById("demo_diary").innerHTML = output;
     }
     else {
@@ -105,17 +105,23 @@ const handleLoginFormSubmit = event => {
     url = API_ENDPOINT + "/users/authenticate";
     event.preventDefault();
     const data = formToJSON(loginform.elements);
+    var loggeduser = data["username"];
     jsondata = JSON.stringify(data, null, "  ");
-    loginform.reset();
 
     ajax_post(url, jsondata, function(data) {
+        var result = data.result
         if (data.status) {
-            document.getElementById("login-feedback").innerHTML = "Login success";
+            var output = "<input type=\"submit\" value=\"Logout\" onclick=\"userLogOut()\">";
+            document.getElementById("login-feedback").innerHTML = "Logged in as " + loggeduser + ". " + output;
+            document.getElementById("token").value = data.result.token;
         }
         else {
             document.getElementById("login-feedback").innerHTML = "Login failed";
+            document.getElementById("token").value = "";
         }
     });
+
+    loginform.reset();
 };
 
 const handleRegisterFormSubmit = event => {
@@ -123,20 +129,85 @@ const handleRegisterFormSubmit = event => {
     event.preventDefault();
     const data = formToJSON(registerform.elements);
     jsondata = JSON.stringify(data, null, "  ");
-    registerform.reset()
 
-    ajax_post(API_ENDPOINT + '/users/register', jsondata, function(data) {
+    ajax_post(url, jsondata, function(data) {
         if (data.status) {
-            document.getElementById("register-feedback").innerHTML = "Register success";
+            document.getElementById("register-feedback").innerHTML = "Successfully registered! Please login to continue.";
         }
         else {
-            document.getElementById("register-feedback").innerHTML = "Register failed";
+            document.getElementById("register-feedback").innerHTML = "Registration failed.";
+        }
+    });
+
+    registerform.reset()
+};
+
+const retrieveDiarySubmit = event => {
+    url = API_ENDPOINT + '/diary'
+    event.preventDefault();
+    const data = formToJSON(retrievediary.elements);
+    jsondata = JSON.stringify(data, null, "  ");
+    document.getElementById("my-diaries").innerHTML = "Please log in to view your diary.";
+
+    ajax_post(API_ENDPOINT + '/diary', jsondata, function(data) {
+        var diaries = data.result;
+        var output = "<ul>";
+        for (var i = 0; i < diaries.length; i++) {
+            entry = diaries[i];
+            temp_date = entry.publish_date;
+            publish_date = temp_date.substring(0, 10)
+            publish_time = temp_date.substring(11, 16)
+            output += "<li>";
+            output += "<strong>" + entry.title + "</strong>";
+            output += "<br>";
+            output += "<small>Written on " + publish_date + " at " + publish_time + "</small>";
+            output += "<hr>" + entry.text + "<hr>";
+            output += "<span><form method=\"post\" id=\"delete-diary\">";
+            output += "<input type=\"hidden\" value=\"" + entry.id + "\" name=\"id\">";
+            output += "<input type=\"submit\" value=\"Delete\" class=\"btn btn-danger\" style=\"float: right\"></form></span>";
+            output += "</li>";
+        }
+        output += "</ul>";
+        if (data.status) {
+            document.getElementById("my-diaries").innerHTML = output;
+        }
+        else {
+            document.getElementById("my-diaries").innerHTML = "Please log in to view your diaries.";
+        }
+    });
+};
+
+const deleteDiarySubmit = event => {
+    url = API_ENDPOINT + "/diary/delete";
+    event.preventDefault();
+    document.getElementById("diary-feedback").innerHTML = "test";
+    saved_data = deletediary.elements;
+    const data = formToJSON(deletediary.elements);
+    jsondata = JSON.stringify(data, null, "  ");
+
+    ajax_post(url, jsondata, function(data) {
+        var result = data.result
+        if (data.status) {
+            document.getElementById("diary-feedback").innerHTML = "Entry deleted";
+        }
+        else {
+            document.getElementById("diary-feedback").innerHTML = "Invalid authentication";
         }
     });
 };
 
 const loginform = document.getElementById('login-form');
 const registerform = document.getElementById('register-form');
-//const data = getFormDataAsJSON(loginform);
+const retrievediary = document.getElementById('retrieve-diary');
+const deletediary = document.getElementById('delete-diary');
+
 loginform.addEventListener('submit', handleLoginFormSubmit);
 registerform.addEventListener('submit', handleRegisterFormSubmit);
+retrievediary.addEventListener('submit', retrieveDiarySubmit);
+deletediary.addEventListener('submit', deleteDiarySubmit);
+
+function userLogOut(){
+    document.getElementById("token").value = "";
+    document.getElementById("my-diaries").innerHTML = "";
+    document.getElementById("login-feedback").innerHTML = "";
+}
